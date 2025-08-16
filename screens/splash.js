778205d26220dbbbc6d7a410ebb764c122790b39
Ayanga Wethmini <wethminiayanga@gmail.com>
2025-08-16 11:01:27 +0530

    added the splash screen

--- screens/splash.js
+++ screens/splash.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import Ripple from "./assets/gifs/ripples.gif"
 
-const App = () => {
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Ripple from '../assets/gifs/ripples.gif';
+
+const SplashScreen = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('Home');
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
   return (
-    <img
-      src={Ripple}
-      alt="Ripple animation"
-      style={{ width: '100%', height: 'auto' }}
-    />
+    <View style={styles.container}>
+      <Image
+        source={Ripple}
+        style={styles.image}
+        resizeMode="contain"
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#030303',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default SplashScreen;
