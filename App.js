d755431468547e00d6c12008c22750d353a55c67
Ayanga Wethmini <wethminiayanga@gmail.com>
2025-08-16 09:41:20 +0530

    Splash screen

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { CreateStackNavigator } from "@react-navigation/stack"
+import SplashScreen from "./screens/splash"
+import HomeScreen from "./screens/home"
 
-export default function App() {
+const Stack = CreateStackNavigator();
+
+
+export default App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+      
+      
+      </Stack.Navigator>
+    
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Ripple from "./assets/gifs/ripples.gif"
+
+const App = () => {
+  return (
+    <img
+      src={Ripple}
+      alt="Ripple animation"
+      style={{ width: '100%', height: 'auto' }}
+    />
+  );
+};
