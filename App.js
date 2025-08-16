f54f1496573bd8634c8cc5deb0ef785bfd0b7851
Ayanga Wethmini <wethminiayanga@gmail.com>
2025-08-16 11:01:57 +0530

    edited the header

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import { CreateStackNavigator } from "@react-navigation/stack"
-import SplashScreen from "./screens/splash"
-import HomeScreen from "./screens/home"
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import SplashScreen from "./screens/splash";
+import HomeScreen from "./screens/home";
 
-const Stack = CreateStackNavigator();
+const Stack = createStackNavigator();
 
 
-export default App() {
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash">
-      
-      
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{
+          headerStyle: { 
+            backgroundColor: '#030303', 
+            height: 120, 
+            borderBottomWidth: 1, 
+            borderBottomColor: '#fff' 
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontSize: 26 }
+        }}
+      >
+        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "My Chats" }} />
       </Stack.Navigator>
-    
     </NavigationContainer>
-  )
+  );
 }
+
+export default App;
